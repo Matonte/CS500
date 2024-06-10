a84b6e080aa86a2fbe7ea635cd6217e873fdc82f
Matonte <matonte3@gmail.com>
2024-06-10 14:25:30 -0500

    Update module5a.py

--- module5a.py
+++ module5a.py
@@ -2,7 +2,8 @@
 # June 10 2024
 # Module 5 problme 1
 #Write a program that uses nested loops to collect data and calculate the average rainfall over a period of years. The program should first ask for the number of years. The outer loop will iterate once for each year. The inner loop will iterate twelve times, once for each month. Each iteration of the inner loop will ask the user for the inches of rainfall for that month. After all iterations, the program should display the number of months, the total inches of rainfall, and the average rainfall per month for the entire period.
-
+# References 
+# https://www.w3schools.com/python/ref_func_range.asp
 # created the global variable
 months = range(12) 
 average = 0.0
