1feb7cc0b7195e410362d6c5e4da4f4e31e0b076
Matonte <matonte3@gmail.com>
2024-06-11 11:40:28 -0500

    Update module5a.py

--- module5a.py
+++ module5a.py
@@ -1,29 +1,39 @@
 # Michael Matonte
 # June 10 2024
-# Module 5 problme 1
-#Write a program that uses nested loops to collect data and calculate the average rainfall over a period of years. The program should first ask for the number of years. The outer loop will iterate once for each year. The inner loop will iterate twelve times, once for each month. Each iteration of the inner loop will ask the user for the inches of rainfall for that month. After all iterations, the program should display the number of months, the total inches of rainfall, and the average rainfall per month for the entire period.
-# References 
-# https://www.w3schools.com/python/ref_func_range.asp
-# created the global variable
-months = range(12) 
-average = 0.0
-# get inputted glolbal variables
-print("How many years do you want to add?")
-years = range(int(input()))
-total_time = 0
-total_rain = 0.0
-average = 0.0 
+# Module 5 problem 2
+#The CSU Global Bookstore has a book club that awards points to its students based on the number of books purchased each month. The points are awarded as follows:
 
-# do the double loop and tally things up
-for n in years:
-    for m in months:
-        print("How much did it rain on " + str(m+1) + "/" + str(n+1) + " ?")
-        total_rain += (float(input()))
-        total_time += 1 
+#    If a customer purchases 0 books, they earn 0 points.
+##    If a customer purchases 2 books, they earn 5 points.
+#    If a customer purchases 4 books, they earn 15 points.
+#    If a customer purchases 6 books, they earn 30 points.
+#    If a customer purchases 8 or more books, they earn 60 points.
+ #   Write a program that asks the user to enter the number of books that they have purchased this month and then display the number of points awarded.
 
-# do the talley 
-average = total_rain/total_time
-print("total recorded time " + str(total_time) + " months")
-print("total recorded rain " + str(total_rain)+ " inches")
-print("average rainfall " + str(average) + "inches")
-  
+#create global variables 
+points = 0
+# input
+# while input in valud
+while(True):
+    #conditional logic for problem. Designed in a way that gives the right amount of points 
+    # in a descending way 
+    try:
+        # tell user they can leave the program
+        print("how many books did you buy? Escape by entering nonnumeric input.")
+        books = int(input())
+        if books > 7:
+            points = 60
+        elif books > 5:
+            points = 30
+        elif books > 3:
+            points = 15
+        elif books > 1:
+            points = 5
+        else: points = 0
+        # print the answer 
+        print("You have earned " + str(points)+".")
+    # leaving the program
+    except ValueError:
+        print("Exiting...")
+        break
+    
